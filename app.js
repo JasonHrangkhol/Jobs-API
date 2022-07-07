ad8410a2e779629328331a42a022472c02fc4bf9
Jason Hrangkhol <hrangkholjason@gmail.com>
2022-07-07 12:11:25 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -23,15 +23,17 @@ app.use(express.json());
 app.use(helmet())
 app.use(cors())
 app.use(xss())
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
 
 //Routes
 app.get('/',(req,res)=>{
   res.send('Jobs API')
 })
+
 app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/jobs',authenticateUser, jobsRouter)
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
